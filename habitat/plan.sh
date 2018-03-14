d5e189a6371309d9ea968c7c564035a890f78040
Lance Finfrock <lfinfrock@chef.io>
2018-03-14 07:20:00 -0700

    binding the php-fpm service
    Signed-off-by: Lance Finfrock <lfinfrock@chef.io>

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -1,7 +1,6 @@
 # This file is the heart of your application's habitat.
 # See full docs at https://www.habitat.sh/docs/reference/plan-syntax/
-pkg_repo=lemp
-pkg_name=lemp-webserver
+pkg_name=lemp
 pkg_origin=lancewf
 pkg_version="0.2.0"
 pkg_maintainer="Lance Finfrock <lfinfrock@chef.io>"
@@ -11,28 +10,20 @@ pkg_source=""
 pkg_svc_user=root
 pkg_svc_group=$pkg_svc_user
 
-pkg_deps=(core/nginx)
+pkg_deps=(core/nginx core/mysql-client)
 
 pkg_binds=(
-  [site]="location"
+  [database]="port username password"
+  [php-fpm]="port bind"
 )
-
-do_download(){
-    return 0
-}
-
-do_verify() {
-  return 0
-}
-
-do_unpack() {
-  return 0
-}
 
 do_build(){
   return 0
 }
 
 do_install() {
-  return 0
+  mkdir $pkg_prefix/static
+  cp -r src/* $pkg_prefix/static/.
+  mkdir $pkg_prefix/database
+  cp -r habitat/database/* $pkg_prefix/database/.
 }
