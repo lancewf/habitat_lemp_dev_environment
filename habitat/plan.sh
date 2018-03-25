419301168411a3cbec88c99d2b218e69a5a5acc9
Lance Finfrock <lfinfrock@chef.io>
2018-03-25 10:21:43 -0700

    Allowing services to run localhost only
    Updated to use a custom mysql and php that have a flag to know if the
    service is bound to localhost only. Also updated this service to be able
    to bind to localhost.
    
    Signed-off-by: Lance Finfrock <lfinfrock@chef.io>

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -12,9 +12,14 @@ pkg_svc_group=$pkg_svc_user
 
 pkg_deps=(core/nginx core/mysql-client)
 
+pkg_exports=(
+  [port]=port
+  [local_only]=local_only
+)
+
 pkg_binds=(
   [database]="port username password local_only"
-  [php]="port"
+  [php]="port local_only"
 )
 
 do_build(){
